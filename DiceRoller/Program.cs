1bfe66ceef6cc151d0f2e9f02551160f46e0ac0d
reidmedlock <benmedlock19@gmail.com>
2023-05-02 20:39:38 -0400

    Add project files.

---
+++ DiceRoller/Program.cs
@@ -0,0 +1,42 @@
+﻿
+{
+    static int DieRoll(int sides)
+    {
+        Random rnd = new Random();
+        int result = rnd.Next(1, sides);
+        return result;
+    }
+}
+
+
+
+int die1;
+int die2;
+
+
+
+
+
+
+
+
+{
+    Random random = new Random();
+        Console.WriteLine("Press any key to roll the die");
+        Console.ReadLine();
+        die1 = random.Next(1, 3);
+        Console.WriteLine("You rolled a " + die1);
+        Console.ReadLine();
+        Console.WriteLine("Press any key to roll the other die");
+        Console.ReadLine();
+        die2 = random.Next(1, 3);
+        Console.WriteLine("You rolled a " + die2);
+        Console.ReadLine();
+        Console.WriteLine("Your total is " + (die1 + die2));
+    }
+    Console.ReadKey();
+
+  
+    
+
+
